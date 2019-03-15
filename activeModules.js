3c875097dcbe3b8d7da6f99060bfa0c3f6fa6a16
Martin Stradiot <martin.stradiot@makers.sk>
2019-03-15 11:50:41 +0100

    create active modules

---
+++ activeModules.js
@@ -0,0 +1,14 @@
+const activeModules = [];
+
+function add({ moduleId, available, info }) {
+    activeModules[moduleId] = {
+      available: data.available,
+      info: data.moduleInfo
+    };
+};
+
+function remove(moduleId) {
+    delete activeModules[moduleId];
+};
+
+module.exports = activeModules;
