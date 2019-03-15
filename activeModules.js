773e769a8fb701c93a6c3256e28ca32234fcae65
Martin Stradiot <martin.stradiot@makers.sk>
2019-03-15 19:34:35 +0100

    bugfix

--- activeModules.js
+++ activeModules.js
@@ -11,8 +11,13 @@ function remove(moduleId) {
     delete activeModules[moduleId];
 };
 
+function update(moduleId, available) {
+    activeModules[moduleId].available = available;
+};
+
 module.exports = {
     activeModules;
     add,
+    update,
     remove
 }
